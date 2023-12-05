1660cd1c4674ed068313252ec4de556ba0095340
DIVYAKV15 <dhivya.kv1992@gmail.com>
2023-12-05 19:25:29 +0300

    inital commit

---
+++ lib/assignments/farmerFreshZoneApp/ScreenSplash.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:practice/assignments/farmerFreshZoneApp/ItemsPage.dart';
+
+class ScreenSplash extends StatefulWidget {
+  const ScreenSplash({super.key});
+
+  @override
+  State<ScreenSplash> createState() => _ScreenSplashState();
+}
+
+class _ScreenSplashState extends State<ScreenSplash> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>ItemsPages()));
+    });
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Image(image: AssetImage("assets/images/farmerFreshzone.png"),),
+      ),
+    );
+  }
+}
---
+++ lib/assignments/farmerFreshZoneApp/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:practice/assignments/farmerFreshZoneApp/ScreenSplash.dart';
+
+void main() {
+  runApp(MyFarmerApp());
+}
+
+class MyFarmerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(home: ScreenSplash());
+  }
+}

7824b111b6c9ba947a668890d07c66e22af6fe5d
DIVYAKV15 <dhivya.kv1992@gmail.com>
2023-12-11 12:11:35 +0300

    inital commit

--- lib/assignments/profileUI/profile_Ui.dart
+++ lib/assignments/profileUI/profile_Ui.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-void main()
-{
-  runApp(MaterialApp(home:ProfileUi()));
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MaterialApp(home: ProfileUi()));
 }
 
 class ProfileUi extends StatelessWidget {
@@ -10,11 +11,89 @@ class ProfileUi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: IconButton(onPressed:(){}, icon: Icon(Icons.arrow_back_ios),),
-        actions: [Drawer(backgroundColor: Colors.black,)],
-
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.arrow_back_ios),
+            color: Colors.black),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.format_align_justify_sharp),
+              color: Colors.black),
+        ],
       ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            bottom: AppBar(
+              title: Container(width: double.infinity,
+                height: 40,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/dhivi.jpg"),
+                      maxRadius: 50,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image(
+                          image: AssetImage(
+                              "assets/icons/social-facebook-button-blue-icon.png"),
+                          height: 30,
+                          width: 50,
+                        ),
+                        Image(
+                          image: AssetImage("assets/icons/Twitter-icon.png"),
+                          height: 30,
+                          width: 50,
+                        ),
+                        Image(
+                          image: AssetImage("assets/icons/linkedin-icon.png"),
+                          height: 30,
+                          width: 50,
+                        ),
+                        Image(
+                          image: AssetImage("assets/icons/web-github-icon.png"),
+                          height: 30,
+                          width: 50,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Text("DhivyaKv",
+                        style: GoogleFonts.racingSansOne(fontSize: 30)),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "@webrror",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "Mobile App Developer",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
 
+        ],
+      ),
     );
   }
 }
--- lib/assignments/profileUI/tabBarProfile.dart
+++ lib/assignments/profileUI/tabBarProfile.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -27,106 +28,128 @@ class TabProfile extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Stack(
           children: [
-            const CircleAvatar(
-              backgroundImage: AssetImage("assets/images/dhivi.jpg"),
-              maxRadius: 50,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Row(
+            Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Image(
-                  image: AssetImage(
-                      "assets/icons/social-facebook-button-blue-icon.png"),
-                  height: 30,
-                  width: 50,
+                const CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/dhivi.jpg"),
+                  maxRadius: 50,
                 ),
-                Image(
-                  image: AssetImage("assets/icons/Twitter-icon.png"),
-                  height: 30,
-                  width: 50,
+                const SizedBox(
+                  height: 10,
                 ),
-                Image(
-                  image: AssetImage("assets/icons/linkedin-icon.png"),
-                  height: 30,
-                  width: 50,
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image(
+                      image: AssetImage(
+                          "assets/icons/social-facebook-button-blue-icon.png"),
+                      height: 30,
+                      width: 50,
+                    ),
+                    Image(
+                      image: AssetImage("assets/icons/Twitter-icon.png"),
+                      height: 30,
+                      width: 50,
+                    ),
+                    Image(
+                      image: AssetImage("assets/icons/linkedin-icon.png"),
+                      height: 30,
+                      width: 50,
+                    ),
+                    Image(
+                      image: AssetImage("assets/icons/web-github-icon.png"),
+                      height: 30,
+                      width: 50,
+                    ),
+                  ],
                 ),
-                Image(
-                  image: AssetImage("assets/icons/web-github-icon.png"),
-                  height: 30,
-                  width: 50,
+                const SizedBox(
+                  height: 15,
+                ),
+                Text("DhivyaKv",
+                    style: GoogleFonts.racingSansOne(fontSize: 30)),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "@webrror",
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Mobile App Developer",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Positioned(
+                  height: 300,
+                  child: CarouselSlider(
+                    items: [
+                      Expanded(
+                        child: ListView(
+                          children: const [
+                            ListTile(
+                              shape: StadiumBorder(),
+                              leading: Icon(Icons.privacy_tip_outlined),
+                              title: Text("Profile"),
+                              trailing: Icon(Icons.arrow_forward_ios),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            ListTile(
+                              shape: StadiumBorder(),
+                              leading: Icon(Icons.history),
+                              title: Text("Purchase History"),
+                              trailing: Icon(Icons.arrow_forward_ios),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            ListTile(
+                              shape: StadiumBorder(),
+                              leading: Icon(Icons.help),
+                              title: Text("Help & Support"),
+                              trailing: Icon(Icons.arrow_forward_ios),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            ListTile(
+                              shape: StadiumBorder(),
+                              leading: Icon(Icons.settings),
+                              title: Text("Settings"),
+                              trailing: Icon(Icons.arrow_forward_ios),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            ListTile(
+                              shape: StadiumBorder(),
+
+                              leading: Icon(Icons.logout),
+                              title: Text("Logout"),
+                              trailing: Icon(Icons.arrow_forward_ios),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                    options: CarouselOptions(
+                        aspectRatio: 6 / 9,
+                        autoPlay: true,
+                        scrollDirection: Axis.vertical,
+                        autoPlayCurve: Curves.elasticInOut),
+                  ),
                 ),
               ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Text("DhivyaKv", style: GoogleFonts.racingSansOne(fontSize: 30)),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "@webrror",
-              style: TextStyle(fontSize: 20),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Mobile App Developer",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-
-            // SizedBox(
-            //   height: 20,
-            // ),
-            ListTile(
-              leading: Icon(Icons.privacy_tip_outlined),
-              title: Text("Profile"),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-            // SizedBox(
-            //   height: 20,
-            // ),
-            ListTile(
-              leading: Icon(Icons.history),
-              title: Text("Purchase History"),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-            // SizedBox(
-            //   height: 20,
-            // ),
-            ListTile(
-              leading: Icon(Icons.help),
-              title: Text("Help & Support"),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-            // SizedBox(
-            //   height: 20,
-            // ),
-            ListTile(
-              shape: StadiumBorder(),
-              leading: Icon(Icons.settings),
-              title: Text("Settings"),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-            // SizedBox(
-            //   height: 20,
-            // ),
-            ListTile(
-              leading: Icon(Icons.person_add_alt),
-              title: Text("Profile"),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text("Logout"),
-              trailing: Icon(Icons.arrow_forward_ios),
             ),
           ],
         ),
